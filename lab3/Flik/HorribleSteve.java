1565fa24d54dabf8e1448d469f9d48c295c69d52
pan990426 <944219135@qq.com>
2022-02-16 10:41:11 +0800

    completed lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -2,7 +2,10 @@ public class HorribleSteve {
     public static void main(String [] args) {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
+            System.out.println("i= " + i + " j= " + j);
             if (!Flik.isSameNumber(i, j)) {
+                System.out.println("j= " + j);
+                System.out.println("i= " + i);
                 break; // break exits the for loop!
             }
         }
